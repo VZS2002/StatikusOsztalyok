f4ebb194a7b42cbef2fd3a7c99ee6675b2ce0324
Varga <vargazsigmond02@gmail.com>
2022-10-06 09:25:10 +0200

    Közös

---
+++ src/hu/petrik/statikusosztalyok/Veletlen.java
@@ -0,0 +1,77 @@
+package hu.petrik.statikusosztalyok;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public final class Veletlen {
+    private Veletlen() {
+    }
+
+    private static final Random rnd = new Random();
+    private static List<String> vezNevek = feltolt("files/veznev.txt");
+    private static List<String> ferfiKerNevek = feltolt("files/ferfikernev.txt");
+    private static List<String> noiKerNevek = feltolt("files/noikernev.txt");
+
+    private static List<String> feltolt(String fajlnev) {
+        List<String> lista = new ArrayList<>();
+        try {
+            Scanner file = null;
+
+            file = new Scanner(new File(fajlnev));
+
+            while (file.hasNext()) {
+                String sor = file.nextLine();
+                lista.add(sor);
+            }
+
+            file.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lista;
+
+    }
+
+
+    public static int velEgesz(int min, int max) {
+        return rnd.nextInt(max - min + 1) + min;
+    }
+
+    public static char velKarakter(char min, char max) {
+        return (char) velEgesz(min, max);
+    }
+
+    public static String velVezetekNev() {
+
+        return vezNevek.get(rnd.nextInt(vezNevek.size()));
+
+    }
+
+
+    public static String velKeresztNev(boolean nem) {
+        String keresztNev;
+        if (nem) {
+            keresztNev = velFerfiKeresztNev();
+        } else {
+            keresztNev = velNoiiKeresztNev();
+        }
+        return keresztNev;
+    }
+
+    private static String velFerfiKeresztNev() {
+
+        return ferfiKerNevek.get(rnd.nextInt(ferfiKerNevek.size()));
+    }
+
+    private static String velNoiiKeresztNev() {
+        return noiKerNevek.get(rnd.nextInt(noiKerNevek.size()));
+    }
+
+    public static String velTeljesNev(boolean nem) {
+        return velVezetekNev() + " " + velKeresztNev(nem);
+    }
+}
