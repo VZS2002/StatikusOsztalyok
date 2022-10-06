c6f44e523fa183c7fe0ca62d32bca5d2619b2085
Varga <vargazsigmond02@gmail.com>
2022-10-06 09:36:22 +0200

    orai

--- src/hu/petrik/statikusosztalyok/Veletlen.java
+++ src/hu/petrik/statikusosztalyok/Veletlen.java
@@ -16,6 +16,9 @@ public final class Veletlen {
     private static List<String> ferfiKerNevek = feltolt("files/ferfikernev.txt");
     private static List<String> noiKerNevek = feltolt("files/noikernev.txt");
 
+    private static List<String> sportagak = feltolt("files/sportag.txt");
+    private static List<String> sportegyesuletek = feltolt("files/egyesulet.txt");
+
     private static List<String> feltolt(String fajlnev) {
         List<String> lista = new ArrayList<>();
         try {
@@ -73,5 +76,29 @@ public final class Veletlen {
 
     public static String velTeljesNev(boolean nem) {
         return velVezetekNev() + " " + velKeresztNev(nem);
+    }
+
+    public static String velDatum(int ev1, int ev2) {
+        int ev = Veletlen.velEgesz(ev1, ev2);
+        int honap = Veletlen.velEgesz(1, 12);
+        int nap = Veletlen.velEgesz(1, 31);
+        return ev + "-" + honap + "-" + nap;
+    }
+
+    public static String velMobil() {
+
+
+        int a = Veletlen.velEgesz(100, 999);
+        int b = Veletlen.velEgesz(11, 99);
+        int c = Veletlen.velEgesz(11, 99);
+        return "+36 (30) " + a + "-" + b + "-" + c;
+    }
+
+    public static String velSportag() {
+        return sportagak.get(rnd.nextInt(sportagak.size()));
+    }
+
+    public static String velSporegyesulet() {
+        return sportegyesuletek.get(rnd.nextInt(sportegyesuletek.size()));
     }
 }
